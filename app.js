8b93d03996642179b368d51230818d0ec9a003ae
JustStewartt <31228677+JustStewartt@users.noreply.github.com>
2021-09-26 17:43:57 -0500

    app

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-require('dotenv').config();
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,7 +10,10 @@ app.use(bodyParser.json());
 /**CONFIGURACIÓN DE MONGOOSE */
 const mongoose = require('mongoose');//Importamos
 //Nos conectamos
-mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.PASSWORD}@cluster0.0l8wy.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`);
+mongoose.connect(
+  process.env.MONGO_URI, // obtiene la url de conexión desde las variables de entorno
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
 //Activamos opción de debuggeo para errores
 mongoose.set("debug", true);
 /**CONFIGURACIÓN DE MONGOOSE */
@@ -28,7 +29,6 @@ require('./config/passport')
 app.use('/v1', require('./routes'));
 app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`)
+})
